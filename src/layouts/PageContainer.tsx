2502336933a100c445e5c10ba7a1608a40fa2df3
Naveen Kumar G <gnknaveen1994@gmail.com>
2025-03-17 19:43:09 +0530

    page issue

--- src/layouts/PageContainer.tsx
+++ src/layouts/PageContainer.tsx
@@ -6,18 +6,22 @@ import { Outlet } from "react-router-dom";
 interface PageContainerProps {
   children: ReactNode;
 }
+
 const PageContainer: React.FC = () => {
   return (
-    <div className="flex flex-col h-screen">
-      <div className="h-16 border border-red-100   w-full bg-gray-100">
+    <div className="flex flex-col h-screen overflow-hidden">
+      {/* Header Section */}
+      <div className="h-16 border border-red-100 w-full bg-gray-100">
         <Header />
       </div>
 
-      <div className="flex-1 overflow-hidden">
+      {/* Main Content Section with Scrollable Area */}
+      <div className="flex-1 overflow-auto w-full min-w-0">
         <Outlet />
       </div>
 
-      <div className="py-2 bg-gray-100 text-center   w-full flex justify-around">
+      {/* Footer Section */}
+      <div className="py-2 bg-gray-100 text-center w-full flex justify-around">
         <NavBar />
       </div>
     </div>
